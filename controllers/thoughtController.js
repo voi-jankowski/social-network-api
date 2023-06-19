046ed3e27bc03c0daa9ac0fc69c1f13e9a1bc884
Voi Jankowski <wojtek.wacek.dola@gmail.com>
2023-06-19 10:22:16 +1000

    Created the rest of the methods for thoughtController.js.

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -39,4 +39,68 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  //   PUT to update a thought by its _id
+  async updateThought({ params, body }, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: params.id },
+        { $set: body },
+        { new: true, runValidators: true }
+      );
+      if (!thought) {
+        res.status(404).json({ message: "There is no thought with this id!" });
+        return;
+      }
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  //   DELETE to remove a thought by its _id
+  async deleteThought({ params }, res) {
+    try {
+      const thought = await Thought.findOneAndDelete({ _id: params.id });
+      if (!thought) {
+        res.status(404).json({ message: "There is no thought with this id!" });
+        return;
+      }
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  //   POST to create a reaction stored in a single thought's reactions array field
+  async createReaction({ params, body }, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: params.thoughtId },
+        { $push: { reactions: body } },
+        { new: true, runValidators: true }
+      );
+      if (!thought) {
+        res.status(404).json({ message: "There is no thought with this id!" });
+        return;
+      }
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  //   DELETE to pull and remove a reaction by the reaction's reactionId value
+  async deleteReaction({ params }, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: params.thoughtId },
+        { $pull: { reactions: { reactionId: params.reactionId } } },
+        { new: true }
+      );
+      if (!thought) {
+        res.status(404).json({ message: "There is no thought with this id!" });
+        return;
+      }
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 };
