81882fb090eb0af2f679f3301a89430a22b6f83f
Voi Jankowski <wojtek.wacek.dola@gmail.com>
2023-06-19 10:16:33 +1000

    Created POST  method for thoughtController.js.

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -12,11 +12,31 @@ module.exports = {
   },
   //   GET to get a single thought by its _id
   async getThoughtById({ params }, res) {
-    const thought = await Thought.findOne({ _id: params.id })
-      .populate({
-        path: "reactions",
-        select: "-__v",
-      })
-      .select("-__v");
+    try {
+      const thought = await Thought.findOne({ _id: params.id })
+        .populate({
+          path: "reactions",
+          select: "-__v",
+        })
+        .select("-__v");
+    } catch (err) {
+      res.status(404).json({ message: "There is no thought with this id!" });
+    }
+  },
+  //   POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+  async createThought({ body }, res) {
+    try {
+      const thought = await Thought.create(body);
+
+      await User.findOneAndUpdate(
+        { _id: body.userId },
+        { $push: { thoughts: thought._id } },
+        { new: true }
+      );
+
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   },
 };
